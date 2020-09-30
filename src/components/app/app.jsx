9072981480578e5ec857a178ed4428fd90244dc2
Сергей Грищенков <grishenkov.sergei@gmail.com>
2020-09-29 20:31:24 -0500

    убрал слеши лишние

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import MainPage from "..//main-page//main-page";
+import MainPage from "../main-page/main-page";
 import PropTypes from "prop-types";
 
 
--- src/components/main-page/main-page.jsx
+++ src/components/main-page/main-page.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import PropTypes from "prop-types";
 
 const MainPage = (props) => {
-
   const {countOffers} = props;
+
   return (
     <div className="page page--gray page--main">
       <header className="header">
