4b4bda737f9c0216a97d9e46887fdff6f20ca39e
Сергей Грищенков <grishenkov.sergei@gmail.com>
2020-10-20 05:50:17 -0500

    внес правки

--- src/components/offer-card/offer-card.jsx
+++ src/components/offer-card/offer-card.jsx
@@ -1,67 +1,69 @@
 import React from "react";
-import {PropTypes4Offer} from "../../propConsts";
+import { PropTypes4Offer } from "../../propConsts";
 import PropTypes from "prop-types";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const Card = (props) => {
-  const {offer, offerId, handleOfferCard} = props;
+  const { offer, offerId, handleOfferCard } = props;
 
-  return (
-    <article
-      data-id={offerId}
-      className="cities__place-card place-card"
-      onMouseOver={(evt) =>
-        handleOfferCard(evt.target.closest(`.cities__place-card`).dataset.id)
-      }
-    >
-      {offer.isPremium ? (
-        <div className="place-card__mark">
-          <span>Premium</span>
-        </div>
-      ) : (
-        ``
-      )}
-      <div className="cities__image-wrapper place-card__image-wrapper">
-        <Link
-          to={{
-            pathname: `/offer/`,
-          }}
-        >
-          <img
-            className="place-card__image"
-            src={offer.photos[0]}
-            width={260}
-            height={200}
-            alt="Place image"
-          />
-        </Link>
-      </div>
-      <div className="place-card__info">
-        <div className="place-card__price-wrapper">
-          <div className="place-card__price">
-            <b className="place-card__price-value">€{offer.price}</b>
-            <span className="place-card__price-text">/&nbsp;night</span>
+  const renderCard = () => {
+    return (
+      <article
+        data-id={offerId}
+        className="cities__place-card place-card"
+        onMouseOver={(evt) =>
+          handleOfferCard(evt.target.closest(`.cities__place-card`).dataset.id)
+        }
+      >
+        {offer.isPremium && (
+          <div className="place-card__mark">
+            <span>Premium</span>
           </div>
-          <button className="place-card__bookmark-button button" type="button">
-            <svg className="place-card__bookmark-icon" width={18} height={19}>
-              <use xlinkHref="#icon-bookmark" />
-            </svg>
-            <span className="visually-hidden">To bookmarks</span>
-          </button>
+        )}
+
+        <div className="cities__image-wrapper place-card__image-wrapper">
+          <Link to="/offer/">
+            <img
+              className="place-card__image"
+              src={offer.photos[0]}
+              width={260}
+              height={200}
+              alt="Place image"
+            />
+          </Link>
         </div>
-        <div className="place-card__rating rating">
-          <div className="place-card__stars rating__stars">
-            <span style={{width: offer.rating}} />
-            <span className="visually-hidden">Rating</span>
+        <div className="place-card__info">
+          <div className="place-card__price-wrapper">
+            <div className="place-card__price">
+              <b className="place-card__price-value">€{offer.price}</b>
+              <span className="place-card__price-text">/&nbsp;night</span>
+            </div>
+            <button
+              className="place-card__bookmark-button button"
+              type="button"
+            >
+              <svg className="place-card__bookmark-icon" width={18} height={19}>
+                <use xlinkHref="#icon-bookmark" />
+              </svg>
+              <span className="visually-hidden">To bookmarks</span>
+            </button>
           </div>
+          <div className="place-card__rating rating">
+            <div className="place-card__stars rating__stars">
+              <span style={{ width: offer.rating }} />
+              <span className="visually-hidden">Rating</span>
+            </div>
+          </div>
+          <h2 className="place-card__name">
+            <a href="#">{offer.description}</a>
+          </h2>
+          <p className="place-card__type">{offer.type}</p>
         </div>
-        <h2 className="place-card__name">
-          <a href="#">{offer.description}</a>
-        </h2>
-        <p className="place-card__type">{offer.type}</p>
-      </div>
-    </article>
-  );
+      </article>
+    );
+  };
+
+  return renderCard();
 };
 
 Card.propTypes = {
